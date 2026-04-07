5cbd29abb8b6dd12c89638b293783861dfa229ca
Heder-souza <hederdesouzasilva@gmail.com>
2026-04-07 08:48:38 -0300

    inicio da aula 4

---
+++ sistema-financeiro/includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Financeiro</title>
+    <link rel="stylesheet" href="/sistema-financeiro/assets/css/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header class="container-fluid bg-dark text-white py-3 mb-4">
+        <div class="container">
+            <h1>Sistema Financeiro</h1>
+        </div>
+    </header>
